fc6c3403c373a8c769991e0a0eba1e8bc090c75f
Daniel Lundin <dln@eintr.org>
2018-06-16 09:59:00 +0200

    Add fan speed

--- nvidia_smi_exporter.go
+++ nvidia_smi_exporter.go
@@ -1,61 +1,60 @@
 package main
 
 import (
-    "flag"
-    "bytes"
-    "encoding/csv"
-    "fmt"
-    "net/http"
-    "log"
-//    "os"
-    "os/exec"
-    "strings"
+	"bytes"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	//    "os"
+	"os/exec"
+	"strings"
 )
-
 
 // name, index, temperature.gpu, utilization.gpu,
 // utilization.memory, memory.total, memory.free, memory.used
 
 var (
-        listenAddress string
-        metricsPath string
+	listenAddress string
+	metricsPath   string
 )
 
 func metrics(response http.ResponseWriter, request *http.Request) {
-    out, err := exec.Command(
-        "nvidia-smi",
-        "--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used",
-        "--format=csv,noheader,nounits").Output()
+	out, err := exec.Command(
+		"nvidia-smi",
+		"--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,fan.speed",
+		"--format=csv,noheader,nounits").Output()
 
-    if err != nil {
-        fmt.Printf("%s\n", err)
-        return
-    }
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
 
-    csvReader := csv.NewReader(bytes.NewReader(out))
-    csvReader.TrimLeadingSpace = true
-    records, err := csvReader.ReadAll()
+	csvReader := csv.NewReader(bytes.NewReader(out))
+	csvReader.TrimLeadingSpace = true
+	records, err := csvReader.ReadAll()
 
-    if err != nil {
-        fmt.Printf("%s\n", err)
-        return
-    }
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
 
-    metricList := []string {
-        "temperature.gpu", "utilization.gpu",
-        "utilization.memory", "memory.total", "memory.free", "memory.used"}
+	metricList := []string{
+		"temperature.gpu", "utilization.gpu",
+		"utilization.memory", "memory.total", "memory.free", "memory.used", "fan.speed"}
 
-    result := ""
-    for _, row := range records {
-        name := fmt.Sprintf("%s[%s]", row[0], row[1])
-        for idx, value := range row[2:] {
-            result = fmt.Sprintf(
-                "%s%s%s{gpu=\"%s\"} %s\n", result, "nvidia.",
-                metricList[idx], name, value)
-        }
-    }
+	result := ""
+	for _, row := range records {
+		name := fmt.Sprintf("%s[%s]", row[0], row[1])
+		for idx, value := range row[2:] {
+			result = fmt.Sprintf(
+				"%s%s%s{gpu=\"%s\"} %s\n", result, "nvidia.",
+				metricList[idx], name, value)
+		}
+	}
 
-    fmt.Fprintf(response, strings.Replace(result, ".", "_", -1))
+	fmt.Fprintf(response, strings.Replace(result, ".", "_", -1))
 }
 
 func init() {
@@ -65,14 +64,14 @@ func init() {
 }
 
 func main() {
-//    addr := ":9101"
-//    if len(os.Args) > 1 {
-//        addr = ":" + os.Args[1]
-//    }
+	//    addr := ":9101"
+	//    if len(os.Args) > 1 {
+	//        addr = ":" + os.Args[1]
+	//    }
 
-    http.HandleFunc(metricsPath, metrics)
-    err := http.ListenAndServe(listenAddress, nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc(metricsPath, metrics)
+	err := http.ListenAndServe(listenAddress, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
