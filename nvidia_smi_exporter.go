b1740925dc192979d34e6aaa2caa8af7fc6930f1
Lars Sjöström <lars@radicore.se>
2018-06-20 12:12:51 +0200

    Add driver version metric

--- nvidia_smi_exporter.go
+++ nvidia_smi_exporter.go
@@ -21,7 +21,7 @@ var (
 func metrics(response http.ResponseWriter, request *http.Request) {
 	out, err := exec.Command(
 		"nvidia-smi",
-		"--query-gpu=name,index,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,fan.speed,power.draw,clocks.current.graphics,clocks.current.sm,clocks.current.memory,clocks.current.video,encoder.stats.sessionCount,encoder.stats.averageFps,encoder.stats.averageLatency",
+		"--query-gpu=name,index,driver_version,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used,fan.speed,power.draw,clocks.current.graphics,clocks.current.sm,clocks.current.memory,clocks.current.video,encoder.stats.sessionCount,encoder.stats.averageFps,encoder.stats.averageLatency",
 		"--format=csv,noheader,nounits").Output()
 
 	if err != nil {
@@ -39,7 +39,7 @@ func metrics(response http.ResponseWriter, request *http.Request) {
 	}
 
 	metricList := []string{
-		"temperature_gpu", "utilization_gpu",
+		"driver_version", "temperature_gpu", "utilization_gpu",
 		"utilization_memory", "memory_total", "memory_free", "memory_used", "fan_speed", "power_draw",
 		"clocks_current_graphics", "clocks_current_sm", "clocks_current_memory", "clocks_current_video",
 		"encoder_stats_session_count", "encoder_stats_average_fps", "encoder_stats_average_latency",
